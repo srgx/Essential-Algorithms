a8b8d8ad4ca61283f2d1b5858bce5d3a2b55841f
Jan Kowalski <yakov85@o2.pl>
2019-11-08 02:02:18 +0100

    Formatting

--- algo.rb
+++ algo.rb
@@ -1,21 +1,21 @@
 def containsDuplicates(array)
-	0.upto(array.size-1) do |i|
-		0.upto(array.size-1) do |j|
-			if(i!=j)
-				if(array[i] == array[j]) then return true end
-			end
-		end
-	end
-	return false
+  0.upto(array.size-1) do |i|
+    0.upto(array.size-1) do |j|
+      if(i!=j)
+        if(array[i] == array[j]) then return true end
+      end
+    end
+  end
+  return false
 end
 
 def containsDuplicates2(array)
-	0.upto(array.size-2) do |i|
-		(i+1).upto(array.size-1) do |j|
-			if(array[i] == array[j]) then return true end
-		end
-	end
-	return false
+  0.upto(array.size-2) do |i|
+    (i+1).upto(array.size-1) do |j|
+      if(array[i] == array[j]) then return true end
+    end
+  end
+  return false
 end
 
 err = "ERROR"
