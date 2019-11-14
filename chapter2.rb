dd306418bda851f9590fb47ea4b2e14b96cc74a1
Jan Kowalski <yakov85@o2.pl>
2019-11-14 13:58:37 +0100

    Exercise 2.12

--- chapter2.rb
+++ chapter2.rb
@@ -282,6 +282,42 @@ if(rectangleRule2(:poly,0,200,25)!=2665600) then raise "Error" end
 
 
 # Exercise 12
-# ...
+# Number of steps grows very slow
+
+def gcdSteps(m,n)
+  steps=0
+  while(n!=0)
+    steps+=1
+    r=m%n
+    m=n
+    n=r
+  end
+  return steps
+end
+
+
+def generatePairs(n)
+  pairs = []
+  n.times do
+    p = []
+    2.times { p << rand(1..100000) }
+    pairs << p
+  end
+  return pairs
+end
+
+def stepsAv(n)
+  pairs = generatePairs(n)
+  result = []
+  pairs.each do |p|
+    c=[]
+    average = (p[0]+p[1])/2.0
+    steps = gcdSteps(p[0],p[1])
+    c << average
+    c << steps
+    result << c
+  end
+  return result
+end
 
 
