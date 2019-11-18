78ec92f4b0f08c917f3f6425a8c91e83404e587f
Jan Kowalski <yakov85@o2.pl>
2019-11-18 17:56:43 +0100

    Exercise 2.11

--- chapter2.rb
+++ chapter2.rb
@@ -150,9 +150,7 @@ def exponentiation(a,p)
   return product
 end
 
-
-T = 200
-
+T = 100
 
 0.upto(T) do |i|
   1.upto(T) do |j|
@@ -160,9 +158,40 @@ T = 200
   end
 end
 
-
 # Exercise 11
-# ...
+
+def modExp(a,p,m)
+  n=1
+  arr=[]
+  while(n<=p)
+    arr << a
+    a=a**2
+    n*=2
+  end
+  n/=2
+  r=n
+  i=arr.size-1
+  product=arr[i]%m # modified
+  while(r<p)
+    while(r+n>p)
+      n/=2
+      i-=1
+    end
+    r+=n
+    product=(product*arr[i])%m # modified
+  end
+  return product
+end
+
+
+0.upto(T) do |i|
+  1.upto(T) do |j|
+    1.upto(T) do |k|
+      if modExp(i,j,k)!=(i**j)%k then raise "Error for values #{i},#{j}" end
+    end
+  end
+end
+
 
 
 # Exercise 12
