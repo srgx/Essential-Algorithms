3a1a205ba4828c80a65b2c25fdc620f467c27fd0
Jan Kowalski <yakov85@o2.pl>
2019-11-28 00:39:14 +0100

    Stack insertion sort

--- chapter5.rb
+++ chapter5.rb
@@ -26,53 +26,60 @@ def reverseStack(stackSentinel)
   return newStack
 end
 
-
-aStack = Cell.new
-push(aStack,4)
-push(aStack,2)
-push(aStack,5)
-push(aStack,1)
-push(aStack,3)
-
-
-# Exercise 3
-
-def popAll(stack)
-  while((v=pop(stack))!=:empty)
-    puts v
+def showStack(stack)
+  while(stack.next!=nil)
+    puts stack.next.value
+    stack=stack.next
   end
 end
 
 
+def stackSize(stack)
+  s=0
+  while(stack.next!=nil)
+    s+=1
+    stack=stack.next
+  end
+  return s
+end
+
+ERR="Error"
+
+
+aStack = Cell.new
+push(aStack,5)
+push(aStack,3)
+push(aStack,6)
+push(aStack,2)
+push(aStack,4)
+push(aStack,7)
+push(aStack,1)
+
+# Exercise 3
+
 def stackInsertionSort(stackSentinel)
   temp_stack=Cell.new
-  num_items=5
+  num_items=stackSize(stackSentinel)
   0.upto(num_items-1) do |i|
     next_item=pop(stackSentinel)
     (num_items-i-1).times do
       g=pop(stackSentinel)
       push(temp_stack,g)
     end
-    
-    set=false
+    while((v=pop(stackSentinel))!=:empty&&v<next_item)
+      push(temp_stack,v)
+    end
+    if(v!=:empty) then push(stackSentinel,v) end
+    push(stackSentinel,next_item)
     while((v=pop(temp_stack))!=:empty)
-      if(v<next_item&&!set)
-        push(stackSentinel,next_item)
-        set=true
-      end
       push(stackSentinel,v)
     end
-    if(!set) then push(stackSentinel,next_item) end
   end
-  
 end
-
 
 stackInsertionSort(aStack)
 
-#popAll aStack
-
-
-
-
+1.upto(stackSize(aStack)) do |i|
+  if(pop(aStack)!=i) then raise ERR end
+end
 
