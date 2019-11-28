c3b6f00ac90766341344306b06e6063b1ffb9caa
Jan Kowalski <yakov85@o2.pl>
2019-11-28 23:20:38 +0100

    Better stack insertion sort

--- chapter5.rb
+++ chapter5.rb
@@ -75,42 +75,67 @@ end
 ERR="Error"
 
 
-aStack = Cell.new
-push(aStack,5)
-push(aStack,3)
-push(aStack,6)
-push(aStack,2)
-push(aStack,4)
-push(aStack,7)
-push(aStack,1)
-
-# Exercise 3
+# Exercises 3, 4
 
 def stackInsertionSort(stackSentinel)
   temp_stack=Cell.new
   num_items=stackSize(stackSentinel)
   0.upto(num_items-1) do |i|
     next_item=pop(stackSentinel)
-    (num_items-i-1).times do
+    (num_items-i-1).times do # move all items to temp
       g=pop(stackSentinel)
       push(temp_stack,g)
     end
-    while((v=pop(stackSentinel))!=:empty&&v<next_item)
+    while((v=pop(stackSentinel))!=:empty&&v<next_item) # push some sorted items to temp
       push(temp_stack,v)
     end
     if(v!=:empty) then push(stackSentinel,v) end
     push(stackSentinel,next_item)
-    while((v=pop(temp_stack))!=:empty)
+    while((v=pop(temp_stack))!=:empty) # move all items from temp to stack
       push(stackSentinel,v)
     end
   end
 end
 
+def stackInsertionSort2(stackSentinel)
+  temp_stack=Cell.new
+  num_items=stackSize(stackSentinel)
+  next_item=pop(stackSentinel)
+  (num_items-1).times { push(temp_stack,pop(stackSentinel)) }
+  0.upto(num_items-1) do |i|
+    c=0
+    while((v=pop(stackSentinel))!=:empty&&v<next_item) # push some sorted items to temp
+      push(temp_stack,v)
+      c+=1
+    end
+    if(v!=:empty) then push(stackSentinel,v) end
+    push(stackSentinel,next_item)
+    c.times { push(stackSentinel,pop(temp_stack)) } # move sorted items back from temp
+    next_item=pop(temp_stack)
+  end
+end
+
+
+NUMS=[5,3,6,2,4,7,1]
+
+aStack=Cell.new
+NUMS.each { |i| push(aStack,i)}
 stackInsertionSort(aStack)
 
+if(stackSize(aStack)!=7) then raise ERR end
 1.upto(stackSize(aStack)) do |i|
   if(pop(aStack)!=i) then raise ERR end
 end
+
+aStack=Cell.new
+NUMS.each { |i| push(aStack,i)}
+stackInsertionSort2(aStack)
+
+if(stackSize(aStack)!=7) then raise ERR end
+1.upto(stackSize(aStack)) do |i|
+  if(pop(aStack)!=i) then raise ERR end
+end
+
 
 # Exercise 5
 # It is possible to sort train with one holding track and output track (for current item)
