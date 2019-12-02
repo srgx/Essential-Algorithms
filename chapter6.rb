b93b6f08c920d652bce9a85a8578cc195a928fc0
Jan Kowalski <yakov85@o2.pl>
2019-12-02 16:17:57 +0100

    Better bubble sort

--- chapter6.rb
+++ chapter6.rb
@@ -26,6 +26,12 @@ def insertionSort(array)
   end
 end
 
+# Exercise 2
+# List of size 1 is already sorted so algorithm can start with index 1
+# Run time is O(N^2)
+
+
+# Exercise 3
 def selectionSort(array)
   0.upto(array.size-1) do |i|
     min_indx=i
@@ -36,6 +42,11 @@ def selectionSort(array)
   end
 end
 
+# Exercise 4
+# Last element is in correct place so algorithm can end at index=size-2.
+# Run time is O(N^2)
+
+# Exercise 5
 def bubbleSort(array)
   swp=true
   while(swp)
@@ -70,6 +81,44 @@ def isSortedDesc(array)
   end
   return sorted
 end
+
+
+# Exercise 6
+
+def betterBubbleSort(array)
+  swp=true
+  from,to=0,array.size-2
+  step=1
+  direction=:up
+  while(swp)
+    swp=false
+    i=from
+    if(direction==:up)
+      while(i<=to)
+        if(array[i]>array[i+1])
+          swap(array,i,i+1)
+          swp=true
+          last=i
+        end
+        i+=step
+      end
+    else
+      while(i>=to)
+        if(array[i]>array[i+1])
+          swap(array,i,i+1)
+          swp=true
+          last=i
+        end
+        i+=step
+      end
+    end
+    from,to=to,from
+    from=last
+    step*=-1
+    direction = direction==:up ? :down : :up
+  end
+end
+
 
 ERR="Error"
 
@@ -107,6 +156,7 @@ puts "Some sort - #{elapsed}"
 unless(isSortedAsc(arr)) then raise ERR end
 
 
+
 arr=[]
 ARRAY_SIZE.times { arr << rand(100) }
 starting= Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -116,6 +166,16 @@ elapsed = ending - starting
 puts "Bubble sort - #{elapsed}"
 
 unless(isSortedAsc(arr)) then raise ERR end
+    
 
+arr=[]
+ARRAY_SIZE.times { arr << rand(100) }
+starting= Process.clock_gettime(Process::CLOCK_MONOTONIC)
+betterBubbleSort(arr)
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+elapsed = ending - starting
+puts "Better bubble sort - #{elapsed}"
+
+unless(isSortedAsc(arr)) then raise ERR end
 
 
