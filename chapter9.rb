da0b68ee133d82af67034a7055f9d6ef057a2a0c
Jan Kowalski <yakov85@o2.pl>
2020-01-03 11:50:46 +0100

    Sierpiński Curve

--- chapter9.rb
+++ chapter9.rb
@@ -156,6 +156,80 @@ end
 #hilbert(4,20,0,Point.new(20,20))
 #show
 
+# Exercises 8, 9
+
+def sierpRight(depth,dx,dy,cr)
+  if(depth>0)
+    depth = depth - 1
+    cr = sierpRight(depth,dx,dy,cr)
+    cr = drawRelative(dx,dy,cr)
+    cr = sierpDown(depth,dx,dy,cr)
+    cr = drawRelative(2*dx,0,cr)
+    cr = sierpUp(depth,dx,dy,cr)
+    cr = drawRelative(dx,-dy,cr)
+    cr = sierpRight(depth,dx,dy,cr)
+  end
+  return cr
+end
+
+def sierpDown(depth,dx,dy,cr)
+  if(depth>0)
+    depth = depth - 1
+    cr = sierpDown(depth,dx,dy,cr)
+    cr = drawRelative(-dx,dy,cr)
+    cr = sierpLeft(depth,dx,dy,cr)
+    cr = drawRelative(0,2*dy,cr)
+    cr = sierpRight(depth,dx,dy,cr)
+    cr = drawRelative(dx,dy,cr)
+    cr = sierpDown(depth,dx,dy,cr)
+  end
+  return cr
+end
+
+def sierpLeft(depth,dx,dy,cr)
+  if(depth>0)
+    depth = depth - 1
+    cr = sierpLeft(depth,dx,dy,cr)
+    cr = drawRelative(-dx,-dy,cr)
+    cr = sierpUp(depth,dx,dy,cr)
+    cr = drawRelative(-2*dx,0,cr)
+    cr = sierpDown(depth,dx,dy,cr)
+    cr = drawRelative(-dx,dy,cr)
+    cr = sierpLeft(depth,dx,dy,cr)
+  end
+  return cr
+end
+
+def sierpUp(depth,dx,dy,cr) #
+  if(depth>0)
+    depth = depth - 1
+    cr = sierpUp(depth,dx,dy,cr)
+    cr = drawRelative(dx,-dy,cr)
+    cr = sierpRight(depth,dx,dy,cr)
+    cr = drawRelative(0,-2*dy,cr)
+    cr = sierpLeft(depth,dx,dy,cr)
+    cr = drawRelative(-dx,-dy,cr)
+    cr = sierpUp(depth,dx,dy,cr)
+  end
+  return cr
+end
+
+
+
+def sierpinski(depth,dx,dy,cr)
+  cr = sierpRight(depth,dx,dy,cr)
+  cr = drawRelative(dx,dy,cr)
+  cr = sierpDown(depth,dx,dy,cr)
+  cr = drawRelative(-dx,dy,cr)
+  cr = sierpLeft(depth,dx,dy,cr)
+  cr = drawRelative(-dx,-dy,cr)
+  cr = sierpUp(depth,dx,dy,cr)
+  drawRelative(dx,-dy,cr)
+end
+
+#sierpinski(4,12,12,Point.new(100,100))
+#show
+
 # ----------------------------------------------------------------
 # TESTS
 # ----------------------------------------------------------------
