45c0063acf18ea9e547362fbb9cd930b4fe68597
Jan Kowalski <yakov85@o2.pl>
2020-01-23 13:11:05 +0100

    Selections and Permutations

--- chapter9-3.rb
+++ chapter9-3.rb
@@ -103,3 +103,127 @@ runTour(5,5)
 puts "-------------------------"
 runWarnsdorf(5,5)
 =end
+
+# Exercise 17
+# Rearranged selections are permutations
+
+# Exercise 18
+LENGTH = 3
+
+def SelectKofNwithDuplicates(index,selections,items,results)
+  if(index==selections.size)
+    result = []
+    0.upto(selections.size-1) do |i|
+      result << items[selections[i]]
+    end
+    results << result
+  else
+    start = 0
+    if(index>0) then start = selections[index-1] end
+    start.upto(items.size-1) do |i|
+      selections[index] = i
+      SelectKofNwithDuplicates(index+1,selections,items,results)
+    end
+  end
+end
+
+def SelectKofNwithoutDuplicates(index,selections,items,results)
+  if(index==selections.size)
+    result = []
+    0.upto(selections.size-1) do |i|
+      result << items[selections[i]]
+    end
+    results << result
+  else
+    start = 0
+    if(index>0) then start = selections[index-1] + 1 end
+    start.upto(items.size-1) do |i|
+      selections[index] = i
+      SelectKofNwithoutDuplicates(index+1,selections,items,results)
+    end
+  end
+end
+
+
+# Exercise 19
+
+def PermuteKofNwithDuplicates(index,selections,items,results)
+  if(index==selections.size)
+    result = []
+    0.upto(selections.size-1) do |i|
+      result << items[selections[i]]
+    end
+    results << result
+  else
+    0.upto(items.size-1) do |i|
+      selections[index] = i
+      PermuteKofNwithDuplicates(index+1,selections,items,results)
+    end
+  end
+end
+
+
+def PermuteKofNwithoutDuplicates(index,selections,items,results)
+  if(index==selections.size)
+    result = []
+    0.upto(selections.size-1) do |i|
+      result << items[selections[i]]
+    end
+    results << result
+  else
+    0.upto(items.size-1) do |i|
+      used = false
+      0.upto(index-1) do |j|
+        if(selections[j] == i) then used = true end
+      end
+      if(!used)
+        selections[index] = i
+        PermuteKofNwithoutDuplicates(index+1,selections,items,results)
+      end
+    end
+  end
+end
+
+
+# TESTS
+# ---------------------------------
+
+ERR = "Error"
+
+# SELECTIONS
+
+a = []
+b = []
+SelectKofNwithDuplicates(0,Array.new(2,0),[4,5,6],b)
+raise ERR if b!= [[4, 4], [4, 5], [4, 6], [5, 5], [5, 6], [6, 6]]
+
+a = []
+b = []
+SelectKofNwithoutDuplicates(0,Array.new(2,0),[4,5,6],b)
+raise ERR if b!= [[4, 5], [4, 6], [5, 6]]
+
+# PERMUTATIONS
+
+a = []
+b = []
+PermuteKofNwithDuplicates(0,Array.new(3,0),[4,5,6],b)
+raise ERR if b!= [[4, 4, 4], [4, 4, 5], [4, 4, 6],
+                  [4, 5, 4], [4, 5, 5], [4, 5, 6],
+                  [4, 6, 4], [4, 6, 5], [4, 6, 6],
+                  [5, 4, 4], [5, 4, 5], [5, 4, 6],
+                  [5, 5, 4], [5, 5, 5], [5, 5, 6],
+                  [5, 6, 4], [5, 6, 5], [5, 6, 6],
+                  [6, 4, 4], [6, 4, 5], [6, 4, 6],
+                  [6, 5, 4], [6, 5, 5], [6, 5, 6],
+                  [6, 6, 4], [6, 6, 5], [6, 6, 6]]
+
+a = []
+b = []
+PermuteKofNwithoutDuplicates(0,Array.new(3,0),[4,5,6],b)
+raise ERR if b != [[4, 5, 6], [4, 6, 5],
+                   [5, 4, 6], [5, 6, 4],
+                   [6, 4, 5], [6, 5, 4]]
+
+
+# Exercise 20
+# ...
