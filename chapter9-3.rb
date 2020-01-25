c02279dcce4a3e7040d0fda8ec525c28964b27c8
Jan Kowalski <yakov85@o2.pl>
2020-01-25 13:45:14 +0100

    Fibonacci

--- chapter9-3.rb
+++ chapter9-3.rb
@@ -218,13 +218,43 @@ def nonRecFibo(n)
     ($maxN+1).upto(n) do |i|
       $fiboValues[i] = nonRecFibo(i-1) + nonRecFibo(i-2)
     end
+    $maxN = n
   end
   return $fiboValues[n]
 end
 
 # Exercise 23
-# ...
 
+def nonRecFiboLast(n)
+  s, b, result = 0, 1, 0
+  2.upto(n) do |i|
+    result = s + b
+    s, b = b, result
+  end
+  return result
+end
+
+def testFibos(n,tests)
+  initFibo
+  starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  tests.times { fibo(n) }
+  ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  puts "Fibo1: #{ending-starting}"
+
+
+  initFibo
+  starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  tests.times { nonRecFibo(n) }
+  ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  puts "Fibo2: #{ending-starting}"
+
+  starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  tests.times { nonRecFiboLast(n) }
+  ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  puts "Fibo3: #{ending-starting}"
+end
+
+#testFibos(1000,1000)
 
 # TESTS
 # ---------------------------------
@@ -266,4 +296,5 @@ raise ERR if b != [[4, 5, 6], [4, 6, 5],
 # FIBONACCI
 initFibo
 raise ERR if fibo(7)!=13
+initFibo
 raise ERR if nonRecFibo(7)!=13
