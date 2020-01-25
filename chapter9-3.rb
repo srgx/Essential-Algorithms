37e7aac86ad27909d13c83bf6f9a2b745bb3e0ef
Jan Kowalski <yakov85@o2.pl>
2020-01-25 14:34:39 +0100

    General Recursion Removal

--- chapter9-3.rb
+++ chapter9-3.rb
@@ -256,6 +256,110 @@ end
 
 #testFibos(1000,1000)
 
+# Exercise 24
+
+class Point
+  attr_accessor :x, :y
+  def initialize(x=0,y=0)
+    @x=x
+    @y=y
+  end
+end
+
+require 'ruby2d'
+
+set background: 'white'
+set fullscreen: true
+
+set width: 1920
+set height: 1080
+set viewport_width: 1920
+set viewport_height: 1080
+
+def drawRelative(x,y,current)
+  targetX = current.x + x
+  targetY = current.y + y
+  Line.new(
+    x1: current.x, y1: current.y,
+    x2: targetX, y2: targetY,
+    width: 3,
+    color: 'fuchsia',
+    z: 20
+  )
+
+  return Point.new(targetX,targetY)
+end
+
+def hilbert(depth,dx,dy,current)
+  sections = []
+  depths = []
+  dxs = []
+  dys = []
+
+  section = 1
+
+  while(section>0)
+    if(section==1)
+      section+=1
+      if(depth>0)
+        sections.push(section)
+        depths.push(depth)
+        dxs.push(dx)
+        dys.push(dy)
+        depth-=1
+        dx, dy = dy, dx
+        section = 1
+      end
+    elsif(section==2)
+      current = drawRelative(dx,dy,current)
+      section+=1
+      if(depth>0)
+        sections.push(section)
+        depths.push(depth)
+        dxs.push(dx)
+        dys.push(dy)
+        depth-=1
+        section = 1
+      end
+    elsif(section==3)
+      current = drawRelative(dy,dx,current)
+      section+=1
+      if(depth>0)
+        sections.push(section)
+        depths.push(depth)
+        dxs.push(dx)
+        dys.push(dy)
+        depth-=1
+        section = 1
+      end
+    elsif(section==4)
+      current = drawRelative(-dx,-dy,current)
+      section+=1
+      if(depth>0)
+        sections.push(section)
+        depths.push(depth)
+        dxs.push(dx)
+        dys.push(dy)
+        depth-=1
+        dx, dy = -dy, -dx
+        section = 1
+      end
+    elsif(section==5)
+      if(sections.empty?)
+        section = -1
+      else
+        section = sections.pop
+        depth = depths.pop
+        dx = dxs.pop
+        dy = dys.pop
+      end
+    end
+  end
+end
+
+#hilbert(4,20,0,Point.new(20,20))
+#show
+
 # TESTS
 # ---------------------------------
 ERR = "Error"
