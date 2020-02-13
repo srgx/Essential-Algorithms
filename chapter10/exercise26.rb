ef584dd402ae747f2bc33b4e0faf23e44bf6eff3
Jan Kowalski <yakov85@o2.pl>
2020-02-13 23:02:45 +0100

    Quadtrees

--- chapter10/exercise26.rb
+++ chapter10/exercise26.rb
@@ -1,6 +1,7 @@
 # Exercise 26
 
 require 'ruby2d'
+require 'set'
 
 set background: 'white'
 set fullscreen: true
@@ -9,6 +10,19 @@ set width: 1920
 set height: 1080
 set viewport_width: 1920
 set viewport_height: 1080
+
+RADIUS = 20
+
+class Point
+  attr_reader :x, :y
+  def initialize(x,y)
+    @x, @y = x, y
+  end
+end
+
+class Result
+  attr_accessor :result
+end
 
 class Rectangle
   def left
@@ -39,34 +53,91 @@ class QuadtreeNode
     @area = area # rectangle
     @xMid = (@area.left + @area.right) / 2.0
     @yMid = (@area.top + @area.bot) / 2.0
-    @items = []
+    @items = [] # leaf node
+  end
+
+  def findLeaf(target)
+    if(@items.nil?)
+      if(target.y<@yMid)
+        if(target.x<@xMid)
+          return @NWchild.findLeaf(target)
+        else
+          return @NEchild.findLeaf(target)
+        end
+      else
+        if(target.x<@xMid)
+          return @SWchild.findLeaf(target)
+        else
+          return @SEchild.findLeaf(target)
+        end
+      end
+    else
+      return self
+    end
+  end
+
+  # find leaves that contain 4 corners of rectangle around target
+  def findLeaves(target)
+    nodes = Set.new
+    nodes << findLeaf(Point.new(target.x-RADIUS,target.y-RADIUS)) # topLeft corner
+    nodes << findLeaf(Point.new(target.x+RADIUS,target.y-RADIUS)) # topRight corner
+    nodes << findLeaf(Point.new(target.x-RADIUS,target.y+RADIUS)) # botLeft corner
+    nodes << findLeaf(Point.new(target.x+RADIUS,target.y+RADIUS)) # botRight corner
+    return nodes
+  end
+
+  def findInLeaf(target,result)
+    @items.each do |itm|
+      distance = Math.sqrt((target.x-itm.x)**2+(target.y-itm.y)**2)
+      if(distance<itm.radius)
+        result.result = itm
+        return true
+      end
+    end
+    return false
+  end
+
+  def findItem(target,result)
+    leaves = self.findLeaves(target) # leaves that may hold item
+    leaves.each do |nd|
+      return true if nd.findInLeaf(target,result)
+    end
+    return false
+  end
+
+  def subdivide
+    wid = (@area.right - @area.left) / 2.0
+    hgt = (@area.bot - @area.top) / 2.0
+
+    # create 4 nodes
+    @NWchild = QuadtreeNode.new(createRectangle(@area.left, @area.top, wid, hgt))
+    @NEchild = QuadtreeNode.new(createRectangle(@area.left + wid,@area.top, wid, hgt))
+    @SEchild = QuadtreeNode.new(createRectangle(@area.left + wid,@area.top + hgt, wid, hgt))
+    @SWchild = QuadtreeNode.new(createRectangle(@area.left,@area.top + hgt, wid,hgt))
+
+    # move items to new nodes
+    @items.each do |itm|
+      if(itm.y<@yMid)
+        if(itm.x < @xMid)
+          @NWchild.addItem(itm)
+        else
+          @NEchild.addItem(itm)
+        end
+      else
+        if(itm.x<@xMid)
+          @SWchild.addItem(itm)
+        else
+          @SEchild.addItem(itm)
+        end
+      end
+    end
+
+    @items = nil # self becomes internal node so @items == nil
   end
 
   def addItem(new_item)
     if((!@items.nil?)&&(@items.size+1>@@maxItems))
-      wid = (@area.right - @area.left) / 2.0
-      hgt = (@area.bot - @area.top) / 2.0
-      @NWchild = QuadtreeNode.new(createRectangle(@area.left, @area.top, wid, hgt))
-      @NEchild = QuadtreeNode.new(createRectangle(@area.left + wid,@area.top, wid, hgt))
-      @SEchild = QuadtreeNode.new(createRectangle(@area.left + wid,@area.top + hgt, wid, hgt))
-      @SWchild = QuadtreeNode.new(createRectangle(@area.left,@area.top + hgt, wid,hgt))
-
-      @items.each do |itm|
-        if(itm.y<@yMid)
-          if(itm.x < @xMid)
-            @NWchild.addItem(itm)
-          else
-            @NEchild.addItem(itm)
-          end
-        else
-          if(itm.x<@xMid)
-            @SWchild.addItem(itm)
-          else
-            @SEchild.addItem(itm)
-          end
-        end
-      end
-      @items = nil
+      self.subdivide
     end
     if(!@items.nil?)
       @items << new_item
@@ -90,9 +161,9 @@ def createLine(x1,y1,x2,y2)
   Line.new(
     x1: x1, y1: y1,
     x2: x2, y2: y2,
-    width: 10,
+    width: 5,
     color: 'black',
-    z: 20
+    z: 15
   )
 end
 
@@ -123,12 +194,28 @@ quad = QuadtreeNode.new(rec)
   y = rand 1080
   circ = Circle.new(
     x: x, y: y,
-    radius: 10,
+    radius: RADIUS,
     sectors: 32,
     color: 'fuchsia',
     z: 20
   )
   quad.addItem(circ)
 end
+
+on :mouse_down do |event|
+  case event.button
+  when :left
+    result = Result.new
+    quad.findItem(Point.new(event.x,event.y),result)
+    if(!result.result.nil?)
+      result.result.color = 'green'
+    end
+  when :middle
+
+  when :right
+
+  end
+end
+
 
 # show
