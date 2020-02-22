b7964a42232849b4bb4e867247d0e17632d8efc4
Jan Kowalski <yakov85@o2.pl>
2020-02-22 23:50:42 +0100

    Minimax

--- chapter12/exercise3.rb
+++ chapter12/exercise3.rb
@@ -9,6 +9,29 @@ def randomMove(board,symbol)
   board[xy[0]][xy[1]] = symbol
 end
 
+class Value
+  attr_accessor :value
+  def initialize(v=nil)
+    @value = v
+  end
+
+  def cp(other)
+    @value = other.value
+  end
+end
+
+class Move
+  attr_accessor :x, :y
+  def initialize(x=nil,y=nil)
+    @x, @y = x, y
+  end
+
+  def cp(other)
+    @x = other.x
+    @y = other.y
+  end
+end
+
 class Board
   def show
     for i in 0..2
@@ -24,6 +47,58 @@ class Board
     end
   end
 end
+
+
+# values: 4 - win, 3 - unknown, 2 - draw, 1 - loss
+def miniMax(board,bestMove,bestValue,player1,player2,depth,maxDepth)
+  if(depth>maxDepth)
+    return Value.new(3)
+  end
+  lowestValue = Float::INFINITY
+  lowestMove = nil
+  for i in 0..2
+    for j in 0..2
+      if(board[i][j].nil?)
+        board[i][j] = player1
+        if(board.win?(player1))
+          lowestValue = 1
+          lowestMove = Move.new(i,j)
+        elsif(board.win?(player2))
+          lowestValue= 4
+          lowestMove = Move.new(i,j)
+        elsif(board.full?)
+          lowestValue = 2
+          lowestMove = Move.new(i,j)
+        else
+          testValue = Float::INFINITY
+          testMove = Move.new
+          miniMax(board,testMove,testValue,player2,player1,depth+1,maxDepth)
+          if(testValue<lowestValue)
+            lowestValue = testValue
+            lowestMove = testMove
+          end
+        end
+        board[i][j] = nil
+      end
+    end
+  end
+  bestMove = lowestMove
+  if(lowestValue == 4)
+    bestValue = 1
+  elsif(lowestValue == 1)
+    bestValue = 4
+  else
+    bestValue = lowestValue
+  end
+end
+
+board = Board.new
+mov = Move.new
+val = Value.new
+miniMax(board,mov,val,"X","O",0,5)
+
+puts "Ruch #{mov.x}-#{mov.y} ma wartość #{val.value}"
+
 
 def playTurn(symbol,player,board)
   if(player==symbol)
@@ -41,7 +116,7 @@ def playTurn(symbol,player,board)
     randomMove(board,symbol)
   end
 end
-
+=begin
 board = Board.new
 winner = nil
 
@@ -82,3 +157,4 @@ if(winner=="X"||winner=="O")
 else
   puts "Remis!"
 end
+=end
