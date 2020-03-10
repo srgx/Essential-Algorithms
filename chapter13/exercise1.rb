c475ae8cbb9029d52f81cc8a3abd727ab0173488
Jan Kowalski <yakov85@o2.pl>
2020-03-10 23:20:25 +0100

    Load nodes

--- chapter13/exercise1.rb
+++ chapter13/exercise1.rb
@@ -38,17 +38,59 @@ Text.new(
   z: 10
 )
 
-fileContent = File.read('test.ntw')
+class Node
+  attr_accessor :name, :x, :y, :links
+  def initialize(name)
+    @name = name
+    @links = []
+  end
+end
+
+class Link
+  attr_accessor :cost, :capacity, :nodes
+  def initialize
+    @nodes = []
+  end
+end
+
+numNodes = nil
+nodes = []
+lines = []
+
+File.open("test.ntw", "r") do |f|
+  numNodes = f.gets.to_i
+  f.each_line do |line|
+    data = line.split
+    lines << data
+    node = Node.new(data[0])
+    node.x, node.y = data[1].to_i, data[2].to_i
+    nodes << node
+  end
+end
 
 
-Text.new(
-  fileContent,
-  x: 750, y: 470,
-  # font: 'vera.ttf',
-  size: 20,
-  color: 'blue',
-  # rotate: 90,
-  z: 10
-)
+for i in 0...numNodes
+  numLinks = (lines[i].size-3)/3
 
-show
+  # add links to node
+  index = 3 # 3 is index of first link in line
+  numLinks.times do
+    link = Link.new
+    link.nodes << nodes[i] # add current node(start)
+    toNodeIndex = lines[i][index].to_i
+    link.nodes << nodes[toNodeIndex] # add target node(destination)
+    link.cost = lines[i][index+1].to_i
+    link.capacity = lines[i][index+2].to_i
+    nodes[i].links << link
+    index += 3 # move to next link
+  end
+end
+
+
+p nodes[0].links
+
+
+
+
+
+# show
