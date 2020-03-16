9c82a20ad36b19ed60f43db792bf1199055d3ab3
Jan Kowalski <yakov85@o2.pl>
2020-03-16 15:13:51 +0100

    Create nodes and links, save networks

--- chapter13/exercise1.rb
+++ chapter13/exercise1.rb
@@ -14,13 +14,21 @@ class Node
   attr_accessor :name, :x, :y, :links
   def initialize(name,x,y)
     @name, @x, @y, @links = name, x, y, []
-    @image = Circle.new(x: @x, y: @y, radius: 40, sectors: 32, color: 'fuchsia', z: 10)
+    @image = Circle.new(x: @x, y: @y, radius: 40, sectors: 32, color: 'fuchsia', z: 8)
     @text = Text.new(@name, x: @x, y: @y, size: 20, color: 'blue', z: 10)
   end
 
   def remove
     [@image,@text].each { |itm| itm.remove }
     @links.each { |lnk| lnk.remove }
+  end
+
+  def activate
+    @image.color = 'green'
+  end
+
+  def deactivate
+    @image.color = 'fuchsia'
   end
 end
 
@@ -33,12 +41,22 @@ class Link
     @image = Line.new(
       x1: @nodes[0].x, y1: @nodes[0].y,
       x2: @nodes[1].x, y2: @nodes[1].y,
-      width: 5, color: 'lime', z: 20
+      width: 5, color: 'lime', z: 8
     )
+
+    midX = (@image.x1 + @image.x2)/2.0
+    midY = (@image.y1 + @image.y2)/2.0
+
+    midX = (@image.x1 + midX)/2.0
+    midY = (@image.y1 + midY)/2.0
+
+
+    @text = Text.new(@cost, x: midX, y: midY, size: 20, color: 'blue', z: 10)
   end
 
   def remove
     @image.remove
+    @text.remove
   end
 end
 
@@ -76,6 +94,11 @@ class Button
       return :new
     elsif(@text=="Load Network")
       return :load
+    elsif(@text=="Save Network")
+      return :save
+    elsif(@text=="Add One Way Link")
+      @image.color = 'green'
+      return :oneway
     end
   end
 
@@ -90,28 +113,76 @@ class State
     @numNodes, @nodes, @buttons, @mode = 0, [], [], :normal
     @buttons << Button.new('New Node',100,50)
     @buttons << Button.new('Load Network',400,50)
+    @buttons << Button.new('Save Network',700,50)
+    @buttons << Button.new('Add One Way Link',100,150)
     @input = TextField.new("test.ntw",1200,50)
+    @temp = []
   end
 
   def click(x,y)
-    option = nil
-    @buttons.each do |btn|
-      if(x>btn.x&&x<btn.x+200&&y>btn.y&&y<btn.y+50)
-        option = btn.click
-        break
+
+    if(@mode==:new)
+      @nodes << Node.new("X",x,y)
+      self.resetMode
+      return
+    elsif(@mode==:oneway)
+      clickedNode = nil
+      @nodes.each do |nd|
+        if((x-nd.x)**2 + (y-nd.y)**2 < 40**2)
+          clickedNode = nd
+          break
+        end
+      end
+      unless(clickedNode.nil?)
+        clickedNode.activate
+        @temp << clickedNode
+        if(@temp.size==2)
+          @temp[0].links << Link.new(@temp[0],@temp[1],1,1)
+          self.resetMode
+        end
+      end
+      return
+    else
+      option = nil
+      @buttons.each do |btn|
+        if(x>btn.x&&x<btn.x+200&&y>btn.y&&y<btn.y+50)
+          option = btn.click
+          break
+        end
+      end
+
+      unless(option.nil?)
+        case option
+        when :load
+          puts "Loading file..."
+          self.load
+        when :new
+          @mode = :new
+          puts "Adding new node..."
+        when :save
+          puts "Saving Network..."
+          self.save
+        when :oneway
+          puts "One Way..."
+          @mode = :oneway
+        end
       end
     end
 
-    unless(option.nil?)
-      case option
-      when :load
-        puts "Loading file..."
-        self.load
-      when :new
-        # @mode = :new
-        puts "Adding new node..."
+  end
+
+  def save
+    content = "#{@nodes.size}\n"
+    nodes.each do |nd|
+      content += "#{nd.name} #{nd.x} #{nd.y}"
+      unless(nd.links.empty?) then content += " " end
+      nd.links.each_with_index do |ln,indx|
+        content += "#{@nodes.index(ln.nodes[1])} #{ln.cost} #{ln.capacity}"
+        if(indx!=nd.links.size-1) then content += " " end
       end
+      content += "\n"
     end
+    File.write(@input.text, content)
   end
 
   def key(ke)
@@ -124,7 +195,7 @@ class State
   end
 
   def load
-    @nodes.each { |nd| nd.remove } # clean images before deleting nodes
+    @nodes.each { |nd| nd.remove } # clear images before deleting nodes
     @nodes, dataSets = [], []
 
     # read data from file to dataSets
@@ -158,6 +229,8 @@ class State
   end
 
   def resetMode
+    @temp.each { |t| t.deactivate }
+    @temp = []
     @mode = :normal
     @buttons.each { | btn | btn.reset }
   end
