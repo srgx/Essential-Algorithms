399227cdc627602e5f67fdb1df3674b54b80236f
Jan Kowalski <yakov85@o2.pl>
2020-04-07 23:35:43 +0200

    Assent

--- chapter15/exercise16.rb
+++ chapter15/exercise16.rb
@@ -1,2 +1,18 @@
 # Exercise 16
-# ...
+
+# Choose path with largest number of diagonals.
+# For figure 15-13 true edit distance is 5.
+
+# assent -> descent
+
+# h - horizontal, v - vertical, d - diagonal
+
+# h(1) -> ssent -> remove 'a'
+# v(1) -> dssent -> add 'd'
+# v(1) -> dessent -> add 'e'
+# d(0) -> dessent -> ---
+# h(1) -> desent -> remove 's'
+# v(1) -> descent -> add 'c'
+# d(0) -> descent -> ---
+# d(0) -> descent -> ---
+# d(0) -> descent -> ---
